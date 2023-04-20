c84f140b011783e2613c490d52ccacd3d7809cf9
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-04-20 11:57:14 +0200

    Next Dashboard

---
+++ components/TopCards.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const TopCards = () => {
+  return (
+    <div className="grid lg:grid-cols-5 gap-4 p-4">
+      <div className="lg:col-span-2 bg-white flex justify-between w-full border p-4 rounded-lg">
+        <div className="flex flex-col w-full pb-4">
+          <p className="text-2xl font-bold">$5.222</p>
+          <p className="text-gray-600">Daily Revenue</p>
+        </div>
+        <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
+          <span className="text-green-700 text-lg">+18%</span>
+        </p>
+      </div>
+      <div className="lg:col-span-2 bg-white flex justify-between w-full border p-4 rounded-lg">
+        <div className="flex flex-col w-full pb-4">
+          <p className="text-2xl font-bold">$75.222</p>
+          <p className="text-gray-600">Monthly Revenue</p>
+        </div>
+        <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
+          <span className="text-green-700 text-lg">+28%</span>
+        </p>
+      </div>
+      <div className=" bg-white flex justify-between w-full border p-4 rounded-lg">
+        <div className="flex flex-col w-full pb-4">
+          <p className="text-2xl font-bold">1222</p>
+          <p className="text-gray-600">Costumers</p>
+        </div>
+        <p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
+          <span className="text-green-700 text-lg">+15%</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default TopCards;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import Sidebar from "@/components/Sidebar";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Sidebar>
+      <Component {...pageProps} />
+    </Sidebar>
+  );
 }
