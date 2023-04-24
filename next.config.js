0a14a6629992e2f48b793b6c5c0d1185cef23b80
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-04-24 13:56:29 +0200

    setting up default page

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true,
+      },
+    ]
+  },
+}
